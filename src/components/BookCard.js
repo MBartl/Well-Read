be656c01a398a10fe3b3f75d7fbf522839e8f637
melonybox <grandmadcow@gmail.com>
2019-06-17 17:22:40 -0400

    Make Search Work

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -4,13 +4,14 @@ import Card from '@material-ui/core/Card';
 class BookCard extends React.Component {
 
     render(){
-      const {imageLinks, title, authors, pageCount} = this.props.book.volumeInfo
+      const {imageLinks, title, authors, pageCount, publishedDate} = this.props.book.volumeInfo
 
       return (
         <Card style={{width: 345, margin: 3}}>
           <img src={imageLinks.thumbnail} alt={title}/>
           <h2>{title}</h2>
-          <h4>{authors.map( author => `"${author}"\n` )}</h4>
+          {authors ? <h4>{authors.map( author => `"${author}"\n` )}</h4> : null}
+          <p>Publish Date: {publishedDate}</p>
           <p>Page Count: {pageCount}</p>
         </Card>
       )
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -2,8 +2,10 @@ import React from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
+import Button from '@material-ui/core/Button';
 import BookListContainer from './BookListContainer.js';
 import { Redirect } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
 
 class MainBox extends React.Component {
   state = {
@@ -20,7 +22,7 @@ class MainBox extends React.Component {
   }
 
   handleFetch = () => {
-    fetch("https://www.googleapis.com/books/v1/volumes?q=japanese+cookbook&orderBy=relevance&key=AIzaSyCvWbjTPUGhdu509kwqKShHyICeEB_JuDw&maxResults=5&startIndex=0&filter=partial")
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.state.searchValue}&orderBy=relevance&key=AIzaSyCvWbjTPUGhdu509kwqKShHyICeEB_JuDw&maxResults=5&startIndex=0&filter=partial`)
     .then(res => res.json())
     .then(data => this.setState({
       bookList: data.items,
@@ -29,9 +31,9 @@ class MainBox extends React.Component {
     .then(() => console.log(this.state))
   }
 
-  componentDidMount(){
-    return this.props.currentUser ? this.handleFetch() : null
-  }
+  // componentDidMount(){
+  //   return this.props.currentUser ? this.handleFetch() : null
+  // }
 
   render(){
     if (this.props.currentUser === null) {
@@ -42,10 +44,15 @@ class MainBox extends React.Component {
     } else {
     return (
       <div>
-        <FormControl>
-          <InputLabel htmlFor="component-simple">Search</InputLabel>
-          <Input id="component-simple" onChange={this.handleChange} value={this.state.searchValue} name="searchValue"/>
-        </FormControl>
+        <container style={{display: "flex",justifyContent: "center",flexDirection: "row",alignItems: "center"}}>
+          <FormControl style={{width: 281}}>
+            <InputLabel htmlFor="component-simple">Search</InputLabel>
+            <Input id="component-simple" onChange={this.handleChange} value={this.state.searchValue} name="searchValue"/>
+          </FormControl>
+          <Button style={{marginTop: "auto"}} onClick={this.handleFetch}>
+            <SearchIcon />
+          </Button>
+        </container>
         <BookListContainer bookList={this.state.bookList} loaded={this.state.loaded}/>
       </div>
     )
